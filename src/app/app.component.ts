7ca6ffeacbed4c5cac04577c0af094ef1cf031a0
Hillary Doan <doanhillary@gmail.com>
2019-06-30 18:01:48 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface ToDo {
+  task: string;
+  completed: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDoListLab1';
+  toDoComponent : ToDo[] = [
+    {
+      task: "Reserve a Uhaul",
+      completed: true,
+    },
+    {
+      task: "Rent a storage unit",
+      completed: true,
+    },
+    {
+      task: "Pack my books",
+      completed: true,
+    },
+    {
+      task: "Organize my art",
+      completed: false,
+    },
+    {
+      task: "Donate old clothes",
+      completed: false,
+    },
+    {
+      task: "Clear out the linen cabinet",
+      completed: false,
+    },
+    {
+      task: "Call city for a special pick-up",
+      completed: false,
+    }
+  ]
+
+
 }
