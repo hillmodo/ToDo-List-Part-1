b77a8b3d8726353af63324f202c262e01fe0762f
Hillary Doan <doanhillary@gmail.com>
2019-07-08 17:00:27 -0400

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ interface ToDo {
   completed: boolean;
 }
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,36 +13,61 @@ interface ToDo {
 })
 export class AppComponent {
   title = 'ToDoListLab1';
-  toDoComponent: ToDo[] = [
-    {
-      task: "Reserve a Uhaul",
-      completed: true,
-    },
-    {
-      task: "Rent a storage unit",
-      completed: true,
-    },
-    {
-      task: "Pack my books",
-      completed: true,
-    },
-    {
-      task: "Organize my art",
-      completed: false,
-    },
-    {
-      task: "Donate old clothes",
-      completed: false,
-    },
-    {
-      task: "Clear out the linen cabinet",
-      completed: false,
-    },
-    {
-      task: "Call city for a special pick-up",
+
+  constructor(){
+    const toDoComponent =[
+      {
+        task: "Reserve a Uhaul",
+        completed: true,
+  
+      },
+      {
+        task: "Rent a storage unit",
+        completed: true,
+      },
+      {
+        task: "Pack my books",
+        completed: true,
+      },
+      {
+        task: "Organize my art",
+        completed: true,
+      },
+      {
+        task: "Donate old clothes",
+        completed: false,
+      },
+      {
+        task: "Clear out the linen cabinet",
+        completed: false,
+      },
+      {
+        task: "Call city for a special pick-up",
+        completed: false,
+      }
+    ]
+    this.toDoComponent = toDoComponent;
+    this.filterToDos = toDoComponent;
+  }
+
+
+  addItem(newTask){
+    let input = {
+      task: newTask,
       completed: false,
     }
-  ]
+    this.toDoComponent.push(input);
+  }
 
+  removeTask(i){
+    this.toDoComponent.splice(i, 1);
+  }
+
+  completeTask(i){
+    this.toDoComponent[i].completed = true;
+  }
+
+  toDoComponent : ToDo[];
+  filterToDos : ToDo[];
 
 }
